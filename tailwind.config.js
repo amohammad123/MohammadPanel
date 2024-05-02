89538fe167ba604b4881591fde85db8e59fe8c37
Mohammad Akhavi <m.h.akhavi@gmail.com>
2024-05-02 22:55:01 +0330

    add init files and build

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily:{
+      body: ['Vazir'],
+      display: ['Vazir'],
+    },
     extend: {},
   },
   plugins: [],
